74248bf11629550b64177830f0d9700fa39a744b
Tedi <75010487+TediKoi@users.noreply.github.com>
2023-08-19 15:14:48 -0400

    adding animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -16,12 +16,14 @@ public class PlayerMovement : MonoBehaviour
     private Transform groundCheck;
     [SerializeField]
     private LayerMask groundLayer;
+    [SerializeField]
+    private Animator animator;
 
 
     // Update is called once per frame
     void Update()
     {
-
+        
         Flip();
 
     }
@@ -50,18 +52,22 @@ public class PlayerMovement : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         horizontal = context.ReadValue<Vector2>().x;
+        animator.SetInteger("isRunning", (int)horizontal);
     }
 
     public void Jump(InputAction.CallbackContext context)
     {
+
         if(context.performed && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            
         }
 
         if(context.canceled && rb.velocity.y > 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            
         }
     }
 }

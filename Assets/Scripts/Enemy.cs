26a005eb84a8242aef37cfa5aaf17584feef1ff4
Tedi <75010487+TediKoi@users.noreply.github.com>
2023-09-01 13:41:31 -0400

    Added enemy patrol

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -11,6 +11,14 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     private Animator animator;
 
+    [Header("Patrol")]
+    [SerializeField]
+    private Transform[] patrolPoints;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private int patrolDestination;
+
 
     // Start is called before the first frame update
     void Start()
@@ -18,12 +26,40 @@ public class Enemy : MonoBehaviour
         currentHealth = maxHealth;
     }
 
+    private void Update()
+    {
+        Patrol();
+    }
+
+    private void Patrol()
+    {
+        if (patrolDestination == 0)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[0].position, speed * Time.deltaTime);
+            //check if he reached destination
+            if (Vector2.Distance(transform.position, patrolPoints[0].position) < .2f)
+            {
+                transform.localScale = new Vector3(-3.6f, 3.6f, 3.6f);
+                patrolDestination = 1;
+            }
+        }
+        if (patrolDestination == 1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[1].position, speed * Time.deltaTime);
+            //check if he reached destination
+            if (Vector2.Distance(transform.position, patrolPoints[1].position) < .2f)
+            {
+                transform.localScale = new Vector3(3.6f, 3.6f, 3.6f);
+                patrolDestination = 0;
+            }
+        }
+    }
+
     public void TakeDmg(int dmg)
     {
         currentHealth -= dmg;
         animator.SetTrigger("isHurt");
-        
-
+       
         if(currentHealth <= 0)
         {
             Die();
@@ -33,6 +69,7 @@ public class Enemy : MonoBehaviour
     void Die()
     {
         Debug.Log("enemy died");
+        animator.SetTrigger("isDead");
         
     }
 }

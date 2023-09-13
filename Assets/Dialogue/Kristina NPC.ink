8cab95f56789df7cf8b7e494a56d1f13af362a81
Tedi <75010487+TediKoi@users.noreply.github.com>
2023-09-12 22:16:46 -0400

    Finished making dialogue system, just need to add story

---
+++ Assets/Dialogue/Kristina NPC.ink
@@ -0,0 +1,15 @@
+-> main
+
+=== main ===
+Hey Joyce!
+We were waiting for you. What took you so long?
+    + [Had to wait for my uber]
+        -> chosen("Had to wait for my uber")
+    + [Wanted to eat before I got on]
+        -> chosen("Wanted to eat before I go on")
+    + [I was tired]
+        -> chosen("I was tired")
+=== chosen(ans) ===
+Oh ok. Well, everyone is waiting for us, we better hurry!
+
+-> END
--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -1,5 +1,6 @@
 using Ink.Runtime;
 using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -14,10 +15,14 @@ public class DialogueManager : MonoBehaviour
     private Animator animator;
 
     private Story currentStory;
-
     public bool dialogueIsPlaying { get; private set; }
-
     private static DialogueManager instance;
+
+    [Header("Choices UI")]
+    [SerializeField] private GameObject[] choices;
+    private TextMeshProUGUI[] choicesText;
+    private List<Choice> currentChoices;
+
 
     private void Awake()
     {
@@ -37,6 +42,14 @@ public class DialogueManager : MonoBehaviour
     {
         dialogueIsPlaying = false;
         
+        //gets all of the choices text
+        choicesText = new TextMeshProUGUI[choices.Length];
+        int index = 0;
+        foreach(GameObject choice in choices)
+        {
+            choicesText[index] = choice.GetComponentInChildren<TextMeshProUGUI>();
+            index++;
+        }
     }
 
     private void Update()
@@ -73,7 +86,10 @@ public class DialogueManager : MonoBehaviour
     {
         if (currentStory.canContinue)
         {
+            //set text for the current dialogue line
             dialogueText.text = currentStory.Continue();
+            //display the choices when available
+            DisplayChoices();
         }
         else
         {
@@ -87,5 +103,36 @@ public class DialogueManager : MonoBehaviour
         yield return new WaitForSecondsRealtime(0.5f);
         animator.SetTrigger("isIdle");
 
+    }
+
+    private void DisplayChoices()
+    {
+        currentChoices = currentStory.currentChoices;
+
+        //checks to make sure our UI has enough buttons for the choices we have
+        if(currentChoices.Count > choices.Length)
+        {
+            print("More choices were given than the UI can support. Number of choices given: " + currentChoices);
+        }
+
+        int index = 0;
+        //enable and start the choices up to the amount of choices for this line of dialogue
+        foreach(Choice choice in currentChoices)
+        {
+            choices[index].gameObject.SetActive(true);
+            choicesText[index].text = choice.text;
+            index++;
+        }
+        //go through the remaining choices the UI supports and make sure they're hidden
+        for(int i = index; i < choices.Length; i++)
+        {
+            choices[i].gameObject.SetActive(false);
+        }
+    }
+
+    public void MakeChoice(int choiceIndex)
+    {
+        currentStory.ChooseChoiceIndex(choiceIndex);
+        ContinueStory();
     }
 }
